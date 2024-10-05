1cf69d605564a0f24dc5960ca0aa03ff9a306573
Irshman <zhenyaskvortsov9@gmail.com>
2024-10-05 12:59:30 +0300

    Add files

---
+++ src/api/skills-image/routes/skills-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skills-image router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::skills-image.skills-image');
---
+++ src/api/skills-image/services/skills-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skills-image service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::skills-image.skills-image');
